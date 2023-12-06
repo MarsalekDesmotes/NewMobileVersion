3e4b2c9c90ab698b4d6ce87a0c391d93ec146bfd
EmirGaziKopar <1190505016@ogrenci.kirklareli.edu.tr>
2023-12-06 12:50:14 +0300

    8.7

--- UnityPhotonFusion/Assets/TextMeshGUIController.cs
+++ UnityPhotonFusion/Assets/TextMeshGUIController.cs
@@ -37,12 +37,15 @@ public class TextMeshGUIController : NetworkBehaviour
         {
             if (counter < 1)
             {
-
                 //health1 = GameObject.FindWithTag("Player2").transform.GetComponent<Health>();
                 health = GameObject.FindWithTag("Player1").transform.GetComponent<Health>();
                 health1 = GameObject.FindWithTag("Player2").transform.GetComponent<Health>();
             }
-    
+
+            //health1 = GameObject.FindWithTag("Player2").transform.GetComponent<Health>();
+            health = GameObject.FindWithTag("Player1").transform.GetComponent<Health>();
+            health1 = GameObject.FindWithTag("Player2").transform.GetComponent<Health>();
+
             textMesh1.text = health.NetworkedHealth.ToString();
             textMesh2.text = health1.NetworkedHealth.ToString();
 
